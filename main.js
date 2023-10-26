54bd981a90010ca59ae859d953a348208cc52db6
tayla11 <132584657+tayla11@users.noreply.github.com>
2023-10-26 17:54:26 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
-
 
 
 const ListaDeTeclas = document.querySelectorAll('.tecla');
 
+let contador = 0;
+
+while (contador < ListaDeTeclas.length) {
+
+    const tecla = ListaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template string
+    const idAudio = `#som_${instrumento}`;
+
+    //console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+
+    //console.log(contador);
+}
